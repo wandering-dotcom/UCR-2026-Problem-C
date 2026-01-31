be4311f626eb28fff22e6eb9741d5269c2b80cd1
wandering-dotcom <dvan037@ucr.edu>
2026-01-31 13:18:03 -0800

    some data cleaning

---
+++ 00_requirements.R
@@ -0,0 +1,31 @@
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("forecast")
+install.packages("lubridate")
+install.packages("knitr")
+install.packages("kableExtra")
+install.packages("webshot")
+install.packages("webshot2")
+install.packages("htmltools")
+install.packages("gridExtra")
+install.packages("grid")
+
+
+# data manipulation
+library(dplyr)
+
+# visualization
+library(ggplot2)
+library(knitr)
+library(kableExtra)
+library(webshot)
+library(webshot2)
+library(htmltools)
+library(gridExtra)
+library(grid)
+
+# prediction
+library(tidyr)
+library(forecast)
+library(lubridate)
